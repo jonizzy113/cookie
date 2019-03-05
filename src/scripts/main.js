5d1d0591dcb164d79faa806426835e283eb82087
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-03-05 11:00:26 -0600

    added index.html and main.js corrected the error

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let x = 1; x < cookies.length; x++) {
+    const currentCookie = cookies[y]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
